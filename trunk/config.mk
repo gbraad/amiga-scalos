b5611b4ceb5b4d72ce7cca4c1f53caa369c4eaef
jlachmann <jlachmann@f9cd8184-2cc5-47b9-8930-ffbfc34bb54f>
2009-03-12 20:33:57 +0000

    Repository reorganization: add missing /trunk /tags /branches

---
+++ trunk/config.mk
@@ -0,0 +1,204 @@
+# $Date$
+# $Revision$
+#############################################################
+
+SHELL	=	/bin/sh
+# On OS4 use gmake to build Scalos
+
+##############################################################################
+
+ifndef MACHINE
+	MACHINE := $(shell gcc $(CFLAGS) -dumpmachine)
+endif
+
+ifeq ($(MACHINE), ppc-morphos)
+
+##############################################################################
+# MorphOS
+##############################################################################
+
+CC 		=	gcc4
+
+ifndef GCCVERSION
+	GCCVERSION := $(shell $(CC) $(CFLAGS) -dumpversion)
+endif
+
+AS 		=	ppc-morphos-as
+LD 		=	ppc-morphos-ld
+AR		=	ppc-morphos-ar
+RANLIB		=	ppc-morphos-ranlib
+STRIP		=	ppc-morphos-strip
+DUMP		=	ppc-morphos-objdump
+
+SVNVERSION	=	$(shell svnversion -n)
+
+CODETYPE	=	PPC
+
+WARNINGS	=	-Wall -Wno-parentheses -Wunused -Wuninitialized -Winline 
+
+ifeq ($(GCCVERSION), 4.0.4)
+WARNINGS	+=	-Wno-pointer-sign
+endif
+
+CPU		=	-mcpu=604e
+#CPU		 +=	 -maltivec
+
+OPTIONS		=	-noixemul -mregnames -mmultiple -mno-prototype -mfused-madd -funsigned-char
+
+OPTIMIZE	=	-O2 -fomit-frame-pointer -fschedule-insns2 -fbranch-count-reg -fno-strict-aliasing
+
+INCLUDES	=	-I$(TOPLEVEL)/ppc-mos-include -I$(TOPLEVEL)/include
+
+DEBUG		=	-ggdb
+
+LFLAGS		=       -L $(TOPLEVEL)/ppc-mos-gcc-lib \
+			-noixemul -laboxstubs -ldebug
+
+CFLAGS		=	$(WARNINGS) $(OPTIMIZE) $(DEBUG) $(CPU) $(OPTIONS) $(INCLUDES) -I.
+
+SFLAGS		=	--remove-section .comment
+
+DFLAGS		=	--syms --reloc --disassemble-all
+
+OBJDIR		=	.obj_mos
+
+############################################################################
+
+DEFINES		=	-D$(CODETYPE) -D__MORPHOS__ -DSVN_VERSION="$(SVNVERSION)"
+
+##############################################################################
+# End of MorphOS
+##############################################################################
+
+else
+ifeq ($(MACHINE), ppc-amigaos)
+
+##############################################################################
+# AmigaOS4
+##############################################################################
+
+CC 		=	ppc-amigaos-gcc
+AS 		=	ppc-amigaos-as
+LD 		=	ppc-amigaos-ld
+AR		=	ppc-amigaos-ar
+RANLIB		=	ppc-amigaos-ranlib
+STRIP		=	ppc-amigaos-strip
+DUMP		=	ppc-amigaos-objdump
+
+SVNVERSION	=	$(shell svnversion -n .)
+
+CODETYPE	=	PPC
+
+WARNINGS	=	-Wall -Wno-parentheses -Wunused -Wuninitialized
+
+CPU		=
+
+OS4_CRT		=	newlib
+
+OPTIONS		=	-mcrt=$(OS4_CRT)
+
+OPTIMIZE	=	-O2 -fomit-frame-pointer -fschedule-insns2 -fbranch-count-reg -fno-strict-aliasing
+
+INCLUDES	=	-I$(TOPLEVEL)/ppc-aos4-include -I$(TOPLEVEL)/include
+
+DEBUG		=	-g
+
+LFLAGS		=       -mcrt=$(OS4_CRT) -L $(TOPLEVEL)/ppc-aos4-gcc-lib \
+			-lm -ldebug
+
+CFLAGS		=	$(WARNINGS) $(OPTIMIZE) $(DEBUG) $(CPU) $(OPTIONS) $(INCLUDES) -I.
+
+SFLAGS		=	--remove-section .comment
+
+DFLAGS		=	--syms --reloc --disassemble-all
+
+OBJDIR		=	.obj_os4
+
+############################################################################
+
+OS4LIBBASE	=	-D__NOLIBBASE__
+DEFINES		=	-D$(CODETYPE) -D__USE_INLINE__ $(OS4LIBBASE) -Dstccpy=strlcpy \
+			-DSVN_VERSION="$(SVNVERSION)"
+
+##############################################################################
+# End of AmigaOS4
+##############################################################################
+
+else
+
+##############################################################################
+# AmigaOS GCC
+##############################################################################
+
+CC 		=	m68k-amigaos-gcc
+#AS 		=	m68k-amigaos-as
+AS		=	SYS:bin/PhxAss
+LD 		=	m68k-amigaos-ld
+AR		=	m68k-amigaos-ar
+RANLIB		=	m68k-amigaos-ranlib
+STRIP		=	m68k-amigaos-strip
+DUMP		=	m68k-amigaos-objdump
+
+CODETYPE	=	M68K
+
+WARNINGS	=	-Wall -Wno-parentheses -Wno-unused -Wno-missing-braces \
+			-Wno-multichar
+
+CPU		=	-m68020-60
+
+OPTIONS		=	-noixemul -msoft-float -fno-strict-aliasing
+
+OPTIMIZE	=	-O2 -fomit-frame-pointer 
+
+INCLUDES	=	-I$(TOPLEVEL)/include -Igg:include -Iinclude:
+
+DEBUG		=	-g
+
+LFLAGS		=	-nostdlib -noixemul -specs=gg:etc/specs -L $(TOPLEVEL)/68k-gcc-lib \
+			-Lgg:lib -Lgg:lib/libnix
+
+CFLAGS		=	$(WARNINGS) $(OPTIMIZE) $(DEBUG) $(CPU) $(OPTIONS) $(INCLUDES) -I.
+
+SFLAGS		=	--strip-all --strip-unneeded --remove-section .comment
+
+DFLAGS	 	=	 --syms --reloc --disassemble-all
+
+ASFLAGS		=	quiet m=68020 linedebug opt=NRQB i=sc:Assembler_Headers/
+
+LIBPATH	 	= 	gg:lib
+
+STARTUP 	= 	$(LIBPATH)/libnix/ncrt0.o $(LIBPATH)/libnix/detach.o gg:lib/asm_debug.o 
+
+OBJDIR		=	.obj_os3
+
+##############################################################################
+
+DEFINES		=	-D$(CODETYPE) -DSVN_VERSION=$(SVNVERSION)
+
+##############################################################################
+# End of AmigaOS GCC
+##############################################################################
+
+endif
+endif
+##############################################################################
+
+#MAKE		 =	 make
+CD		=	cd
+CP		=	cp
+RM		=	rm
+ECHO		=	echo
+MKDIR		=	mkdir
+LN		=	ln
+SED		=	sed
+CATCOMP		=	CatComp
+
+##############################################################################
+
+TOOLSDIR	=	$(TOPLEVEL)/tools
+
+##############################################################################
+
+FIXDEPS		=	$(TOOLSDIR)/fixdeps
+
+##############################################################################
