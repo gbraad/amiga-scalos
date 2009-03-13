6fec6e947f50c4ee7cf1cc47437eb60adf91cadb
jlachmann <jlachmann@f9cd8184-2cc5-47b9-8930-ffbfc34bb54f>
2009-03-13 18:04:01 +0000

    Bumped version to 41.7

--- main/Scalos_rev.h
+++ main/Scalos_rev.h
@@ -9,11 +9,11 @@
 #include <Year.h>
 
 #define VERSION			41
-#define REVISION		6
+#define REVISION		7
 #define	VERS_REV_STR            STR(VERSION) "." STR(REVISION)
-#define	RELEASE			"1.7"
+#define	RELEASE			"1.8"
 #define	BUILDNR			STR(SVN_VERSION)
-#define DATE			"12.08.2007"
+#define DATE			"13.03.2009"
 #define VERSTAG			"\00$VER: Scalos " VERS_REV_STR " release " RELEASE " (" DATE ") " "build " BUILDNR " " COMPILER_STRING
 #define	COPYRIGHT		"\n © 1999" CURRENTYEAR " The Scalos Team\n"
 
