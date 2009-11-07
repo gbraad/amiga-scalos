9e2dd4565aefd93c90293c82f22ca9c3792ed6f0
jlachmann <jlachmann@f9cd8184-2cc5-47b9-8930-ffbfc34bb54f>
2009-11-07 17:08:46 +0000

    Bugfix: corrected version strings.

--- Plugins/OOP/TitleClock/plugin_data.h
+++ Plugins/OOP/TitleClock/plugin_data.h
@@ -16,13 +16,13 @@
 #define LIB_BASETYPE 		struct myLibBase
 
 #define LIB_VERSION		40		/* Version of our plugin */
-#define LIB_REVISION		4		/* Revision of our plugin */
+#define LIB_REVISION		5		/* Revision of our plugin */
 #define NAME			"title_clock"	/* Name of our plugin (must be the same as the executable filename minus the .plugin [gets added later]) */
 #define LIBVER                  STR(LIB_VERSION) "." STR(LIB_REVISION) 		/* part of the version string for our plugin */
 
 #define LIB_NAME 		NAME ".plugin"
 #define LIB_IDSTRING 		NAME LIBVER " " COMPILER_STRING
-#define LIB_VERSTRING 		"$VER: " LIB_NAME LIBVER " (14.04.2005) " COMPILER_STRING \
+#define LIB_VERSTRING 		"\0$VER: " LIB_NAME " " LIBVER " (07.11.2009) " COMPILER_STRING \
 				" ©2001" CURRENTYEAR " The Scalos Team"
 
 #define CI_CLASSNAME 		"Title.sca"
