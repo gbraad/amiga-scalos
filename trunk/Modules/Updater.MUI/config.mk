a5c308db743942304671f4d804d2ab0eb68b549b
jlachmann <jlachmann@f9cd8184-2cc5-47b9-8930-ffbfc34bb54f>
2009-11-13 16:27:08 +0000

    Improvement: added preliminary and unfinished version of online updater module. --- WORK IN PROGRESS --- UNFINISHED ---

---
+++ trunk/Modules/Updater.MUI/config.mk
@@ -0,0 +1,48 @@
+# $Date$
+# §Revision:                 $
+##############################################################################
+
+include $(TOPLEVEL)/config.mk
+include $(TOPLEVEL)/rules.mk
+
+##############################################################################
+
+ICONOBJMCC_DIR	= $(TOPLEVEL)/common/IconobjectMCC
+
+##############################################################################
+# Check gcc
+
+ifeq ($(MACHINE), ppc-morphos)
+
+###############################################################################
+# MorphOS
+
+INCLUDES	+=      -I$(ICONOBJMCC_DIR) \
+
+LFLAGS	+=	\
+		-lcurl \
+		-lz \
+
+
+else
+ifeq ($(MACHINE), ppc-amigaos)
+
+###############################################################################
+# AmigaOS4
+
+INCLUDES	+=      -I$(ICONOBJMCC_DIR) \
+
+LFLAGS	+=	-lauto
+
+
+else
+
+
+###############################################################################
+# AmigaOS
+
+LFLAGS	+=	#
+
+endif
+endif
+
