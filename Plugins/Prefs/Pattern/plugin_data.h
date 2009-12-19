ef50594e61df25010889ff8160f0a2290eb2d4d8
jlachmann <jlachmann@f9cd8184-2cc5-47b9-8930-ffbfc34bb54f>
2009-12-19 12:35:37 +0000

    Bugfix: added missing MUIA_CycleChain attribute for Dither Mode gadget.

--- Plugins/Prefs/Pattern/plugin_data.h
+++ Plugins/Prefs/Pattern/plugin_data.h
@@ -11,7 +11,7 @@
 #define PLUGIN_TYPE	PREFS
 
 #define	LIB_VERSION	40
-#define	LIB_REVISION	20
+#define	LIB_REVISION	21
 
 #define LIB_NAME	"Pattern.prefsplugin"
 #define LIB_VERSTRING   "$VER: " LIB_NAME " " \
