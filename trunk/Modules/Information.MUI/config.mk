9cef6a93c3ee3712a1b2596ea5f7b2fd2b6b6a21
jlachmann <jlachmann@f9cd8184-2cc5-47b9-8930-ffbfc34bb54f>
2010-01-17 13:41:18 +0000

    Improvement: switch information.module to use new FsAbstraction instead of old Dos64.

--- trunk/Modules/Information.MUI/config.mk
+++ trunk/Modules/Information.MUI/config.mk
@@ -10,14 +10,16 @@ include $(TOPLEVEL)/rules.mk
 TOOLTYPE_DIR	=  ../IconProperties.MUI
 ICONOBJMCC_DIR	= $(TOPLEVEL)/common/IconobjectMCC
 INT64_DIR	= $(TOPLEVEL)/common/Int64
+FS_DIR	     	= $(TOPLEVEL)/common/Fs
 
-vpath	%.c	$(TOOLTYPE_DIR) $(ICONOBJMCC_DIR) $(INT64_DIR)
+vpath	%.c	$(TOOLTYPE_DIR) $(ICONOBJMCC_DIR) $(INT64_DIR) $(FS_DIR)
 
 ##############################################################################
 
 INCLUDES	+= 	-I$(TOOLTYPE_DIR) \
 			-I$(ICONOBJMCC_DIR) \
 			-I$(INT64_DIR) \
+			-I$(FS_DIR) \
 
 ##############################################################################
 # Check gcc
