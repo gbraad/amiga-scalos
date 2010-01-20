8b0be0f1bae778bdf15e0775cd049b6ead637b8d
jlachmann <jlachmann@f9cd8184-2cc5-47b9-8930-ffbfc34bb54f>
2010-01-20 18:10:36 +0000

    Improvement: removed obsolete functions from Dos64

--- trunk/common/Int64/int64.h
+++ trunk/common/Int64/int64.h
@@ -36,14 +36,7 @@ void Convert64(const struct Locale *locale, ULONG64 Number, STRPTR Buffer, size_
 ///
 LONG ScalosExamine64(BPTR lock, struct FileInfoBlock *fib);
 LONG ScalosExNext64(BPTR lock, struct FileInfoBlock *fib);
-ULONG64 ScalosSeek64(BPTR file, ULONG64 position, LONG mode);
 ULONG64 ScalosFibSize64(const struct FileInfoBlock *fib);
-ULONG64 ScalosFibNumBlocks64(const struct FileInfoBlock *fib);
-void ScalosSetFibSize64(struct FileInfoBlock *fib, ULONG64 Size);
-ULONG64 ScalosExAllSize64(const struct ExAllData *ead, ULONG edType);
-void ScalosSetExAllSize64(struct ExAllData *ead, ULONG edType, ULONG64 Size);
-void ScalosDosPacketExNext64(struct DosPacket *dp, BPTR lock, struct FileInfoBlock *fib);
-BOOL ScalosSupports64(struct MsgPort *fileSysPort, BPTR dirLock, struct FileInfoBlock *fib);
 ///
 /* ------------------------------------------------- */
 
