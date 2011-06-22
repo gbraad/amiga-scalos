3d164fdb8597dd77a51eb8803c8bc7359e7390a9
mrustler_______ <mrustler_______@f9cd8184-2cc5-47b9-8930-ffbfc34bb54f>
2011-06-22 16:24:29 +0000

    Added missing ttengine headers. I can't test them!

--- include/clib/ttengine_protos.h
+++ include/clib/ttengine_protos.h
@@ -1,60 +1,56 @@
-/* Automatically generated header! Do not edit! */
-
 #ifndef CLIB_TTENGINE_PROTOS_H
 #define CLIB_TTENGINE_PROTOS_H
 
+
 /*
-**	$VER: ttengine_protos.h 7.0 (13.01.2005)
+**  $VER: ttengine_protos.h 7.0 (13.01.2005)
 **
-**	C prototypes. For use with 32 bit integers only.
+**  C prototypes. For use with 32 bit integers only.
 **
-**	Copyright © 2001 Amiga, Inc.
-**	    All Rights Reserved
+**  (c) Grzegorz Kraszewski 2003 - 2005
+**  All Rights Reserved
 */
 
+#ifndef  EXEC_TYPES_H
 #include <exec/types.h>
+#endif
+#ifndef  UTILITY_TAGITEM_H
 #include <utility/tagitem.h>
+#endif
+#ifndef  LIBRARIES_TTENGINE_H
 #include <libraries/ttengine.h>
+#endif
+#ifndef  GRAPHICS_TEXT_H
 #include <graphics/text.h>
+#endif
+#ifndef  GRAPHICS_RASTPORT_H
+#include <graphics/rastport.h>
+#endif
 
-#ifdef __cplusplus
-extern "C" {
-#endif /* __cplusplus */
-
-
-/*--- functions in V4 or higher ---*/
-APTR TT_OpenFontA(struct TagItem *taglist);
+APTR TT_OpenFontA(struct TagItem * taglist);
 APTR TT_OpenFont(Tag tag1, ...);
-BOOL TT_SetFont(struct RastPort *rp, APTR font);
-VOID TT_CloseFont(APTR font);
-VOID TT_Text(struct RastPort *rp, APTR string, ULONG count);
-ULONG TT_SetAttrsA(struct RastPort *rp, struct TagItem *taglist);
-ULONG TT_SetAttrs(struct RastPort *rp, Tag tag1, ...);
-ULONG TT_GetAttrsA(struct RastPort *rp, struct TagItem *taglist);
-ULONG TT_GetAttrs(struct RastPort *rp, Tag tag1, ...);
-ULONG TT_TextLength(struct RastPort *rp, APTR string, ULONG count);
-VOID TT_TextExtent(struct RastPort *rp, APTR string, WORD count, struct TextExtent *te);
-ULONG TT_TextFit(struct RastPort *rp, APTR string, UWORD count, struct TextExtent *te, struct TextExtent *tec, WORD dir, UWORD cwidth, UWORD cheight);
-
-/*--- functions in V5 or higher ---*/
-struct TT_Pixmap * TT_GetPixmapA(APTR font, APTR string, ULONG count, struct TagItem *taglist);
+BOOL TT_SetFont(struct RastPort * rp, APTR font);
+void TT_CloseFont(APTR font);
+void TT_Text(struct RastPort * rp, APTR string, ULONG count);
+ULONG TT_SetAttrsA(struct RastPort * rp, struct TagItem * taglist);
+ULONG TT_SetAttrs(struct RastPort * rp, Tag tag1, ...);
+ULONG TT_GetAttrsA(struct RastPort * rp, struct TagItem * taglist);
+ULONG TT_GetAttrs(struct RastPort * rp, Tag tag1, ...);
+ULONG TT_TextLength(struct RastPort * rp, APTR string, ULONG count);
+void TT_TextExtent(struct RastPort * rp, APTR string, WORD count, struct TextExtent * te);
+ULONG TT_TextFit(struct RastPort * rp, APTR string, UWORD count, struct TextExtent * te,
+  struct TextExtent * tec, WORD dir, UWORD cwidth, UWORD cheight);
+struct TT_Pixmap * TT_GetPixmapA(APTR font, APTR string, ULONG count,
+  struct TagItem * taglist);
 struct TT_Pixmap * TT_GetPixmap(APTR font, APTR string, ULONG count, Tag tag1, ...);
-VOID TT_FreePixmap(struct TT_Pixmap *pixmap);
-VOID TT_DoneRastPort(struct RastPort *rp);
-
-/*--- functions in V6 or higher ---*/
+void TT_FreePixmap(struct TT_Pixmap * pixmap);
+void TT_DoneRastPort(struct RastPort * rp);
 APTR TT_AllocRequest(void);
-struct TagItem* TT_RequestA(APTR request, struct TagItem *taglist);
+struct TagItem* TT_RequestA(APTR request, struct TagItem * taglist);
 struct TagItem* TT_Request(APTR request, Tag tag1, ...);
-VOID TT_FreeRequest(APTR request);
-
-/*--- functions in V7 or higher ---*/
+void TT_FreeRequest(APTR request);
 STRPTR * TT_ObtainFamilyListA(struct TagItem *taglist);
 STRPTR * TT_ObtainFamilyList(Tag tag1, ...);
-VOID TT_FreeFamilyList(STRPTR *list);
+void TT_FreeFamilyList(STRPTR * list);
 
-#ifdef __cplusplus
-}
-#endif /* __cplusplus */
-
-#endif /* CLIB_TTENGINE_PROTOS_H */
+#endif  /*  CLIB_TTENGINE_PROTOS_H  */
