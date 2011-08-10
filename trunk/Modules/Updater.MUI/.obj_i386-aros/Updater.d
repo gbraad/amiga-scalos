37a6a86ab26163da7b8f68b1b868d0b4b2392fda
mrustler_______ <mrustler_______@f9cd8184-2cc5-47b9-8930-ffbfc34bb54f>
2011-08-10 13:09:09 +0000

    AROS port of Updater.module. Note that AROS uses static openssl lib. Can't test it because of network problems :-(.

---
+++ trunk/Modules/Updater.MUI/.obj_i386-aros/Updater.d
@@ -0,0 +1,20 @@
+.obj_i386-aros/Updater.o .obj_i386-aros/Updater.d : Updater.c \
+  /home/mazze/projects/scalos/Modules/Updater.MUI/../../include/scalos/scalos.h \
+  /home/mazze/projects/scalos/Modules/Updater.MUI/../../include/datatypes/iconobject.h \
+  /home/mazze/projects/scalos/Modules/Updater.MUI/../../include/scalos/scalosgfx.h \
+  /home/mazze/projects/scalos/Modules/Updater.MUI/../../include/scalos/undo.h \
+  /home/mazze/projects/scalos/Modules/Updater.MUI/../../include/scalos/int64types.h \
+  /home/mazze/projects/scalos/Modules/Updater.MUI/../../include/curl/curl.h \
+  /home/mazze/projects/scalos/Modules/Updater.MUI/../../include/curl/curlver.h \
+  /home/mazze/projects/scalos/Modules/Updater.MUI/../../include/curl/curlbuild.h \
+  /home/mazze/projects/scalos/Modules/Updater.MUI/../../include/curl/curlrules.h \
+  /home/mazze/projects/scalos/Modules/Updater.MUI/../../include/curl/easy.h \
+  /home/mazze/projects/scalos/Modules/Updater.MUI/../../include/curl/multi.h \
+  /home/mazze/projects/scalos/Modules/Updater.MUI/../../include/curl/curl.h \
+  /home/mazze/projects/scalos/Modules/Updater.MUI/../../include/proto/scalos.h \
+  /home/mazze/projects/scalos/Modules/Updater.MUI/../../include/clib/scalos_protos.h \
+  /home/mazze/projects/scalos/Modules/Updater.MUI/../../include/defines/scalos.h \
+  /home/mazze/projects/scalos/Modules/Updater.MUI/../../include/DefIcons.h \
+  /home/mazze/projects/scalos/Modules/Updater.MUI/../../include/defs.h \
+  /home/mazze/projects/scalos/Modules/Updater.MUI/../../include/Year.h \
+  Updater.h debug.h .obj_i386-aros/Updater_Locale.h
